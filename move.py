de7fee9a06099b1712f2382d1a5fbb26f98cb0bb
Divzro <Charlesm4@gmail.com>
2016-03-01 19:06:36 -0700

    Added PWM

--- move.py
+++ move.py
@@ -16,6 +16,17 @@ GPIO.setup(DRIVE_2, GPIO.OUT)
 GPIO.setup(DRIVE_3, GPIO.OUT)
 GPIO.setup(DRIVE_4, GPIO.OUT)
 
+fwleft = GPIO.PWM(DRIVE_1,50)
+rvleft = GPIO.PWM(DRIVE_2,50)
+fwright = GPIO.PWM(DRIVE_3,50)
+rvright = GPIO.PWM(DRIVE_4,50)
+
+
+fwleft.start(0)
+rvleft.start(0)
+fwright.start(0)
+rvright.start(0)
+
 
 class _Getch:
     def __call__(self):
@@ -39,84 +50,88 @@ class _Getch:
 #j=106
 #l=108
 
-def get():
+def drive(power):
     inkey = _Getch()
     k=inkey()
+    print power
 #FORWARD - W KEY
     if ord(k) == 119:
-                GPIO.output(DRIVE_1, GPIO.HIGH)
-                GPIO.output(DRIVE_2, GPIO.LOW)
-                GPIO.output(DRIVE_3, GPIO.HIGH)
-                GPIO.output(DRIVE_4, GPIO.LOW)
+        if power > 99:
+            print "Full Speed"
+            pass
+        if power < 0:
+            print "Accelerating"
+            power = power + 1
+            rvleft.start(power)
+            rvright.start(power)
+            fwleft.start(0)
+            fwright.start(0)
+        if power > -1:
+            power = power + 1
+            fwleft.start(power)
+            fwright.start(power)
+            rvleft.start(0)
+            rvright.start(0)
 #REVERSE - S KEY
     if ord(k) == 115:
-                GPIO.output(DRIVE_1, GPIO.LOW)
-                GPIO.output(DRIVE_2, GPIO.HIGH)
-                GPIO.output(DRIVE_3, GPIO.LOW)
-                GPIO.output(DRIVE_4, GPIO.HIGH)
+        if power < -99:
+            pass
+            print "Full Reverse"
+        if power < -1:
+            rvleft.start(power)
+            rvright.start(power)
+            power = power - 1
+        if power > 0:
+            power = power - 1
+            fwleft.start(-power)
+            fwright.start(-power)
 #SPIN LEFT - A KEY
     if ord(k) == 97:
-                GPIO.output(DRIVE_1, GPIO.LOW)
-                GPIO.output(DRIVE_2, GPIO.LOW)
-                GPIO.output(DRIVE_3, GPIO.HIGH)
-                GPIO.output(DRIVE_4, GPIO.LOW)
+        if power > 20:
+            fwright.start(power-20)
+            rvright.start(0)
+        if power < 0:
+            fwright.start(0)
+            rvright.start(power-20)
 #SPIN LEFT - E KEY
-    if ord(k) == 101:
-                GPIO.output(DRIVE_1, GPIO.HIGH)
-                GPIO.output(DRIVE_2, GPIO.LOW)
-                GPIO.output(DRIVE_3, GPIO.LOW)
-                GPIO.output(DRIVE_4, GPIO.HIGH)
+#    if ord(k) == 101:
+#                GPIO.output(DRIVE_1, GPIO.HIGH)
+#                GPIO.output(DRIVE_2, GPIO.LOW)
+#                GPIO.output(DRIVE_3, GPIO.LOW)
+#                GPIO.output(DRIVE_4, GPIO.HIGH)
 #SPIN LEFT - Q KEY
-    if ord(k) == 113:
-                GPIO.output(DRIVE_1, GPIO.LOW)
-                GPIO.output(DRIVE_2, GPIO.HIGH)
-                GPIO.output(DRIVE_3, GPIO.HIGH)
-                GPIO.output(DRIVE_4, GPIO.LOW)
+#    if ord(k) == 113:
+#                GPIO.output(DRIVE_1, GPIO.LOW)
+#                GPIO.output(DRIVE_2, GPIO.HIGH)
+#                GPIO.output(DRIVE_3, GPIO.HIGH)
+#                GPIO.output(DRIVE_4, GPIO.LOW)
 #TURN RIGHT - D KEY
     if ord(k) == 100:
-                GPIO.output(DRIVE_1, GPIO.HIGH)
-                GPIO.output(DRIVE_2, GPIO.LOW)
-                GPIO.output(DRIVE_3, GPIO.LOW)
-                GPIO.output(DRIVE_4, GPIO.LOW)
-
-    if ord(k) == 105:
-                GPIO.output(DRIVE_1, GPIO.LOW)
-                GPIO.output(DRIVE_2, GPIO.LOW)
-                GPIO.output(DRIVE_3, GPIO.HIGH)
-                GPIO.output(DRIVE_4, GPIO.HIGH)
-# - L KEY
-    if ord(k) == 108:
-                GPIO.output(DRIVE_1, GPIO.LOW)
-                GPIO.output(DRIVE_2, GPIO.LOW)
-                GPIO.output(DRIVE_3, GPIO.LOW)
-                GPIO.output(DRIVE_4, GPIO.HIGH)
-# - J KEY
-    if ord(k) == 106:
-                GPIO.output(DRIVE_1, GPIO.LOW)
-                GPIO.output(DRIVE_2, GPIO.LOW)
-                GPIO.output(DRIVE_3, GPIO.HIGH)
-                GPIO.output(DRIVE_4, GPIO.LOW)
-#FULL STOP - K Key
-    if ord(k) == 107:
-                GPIO.output(DRIVE_1, GPIO.LOW)
-                GPIO.output(DRIVE_2, GPIO.LOW)
-                GPIO.output(DRIVE_3, GPIO.LOW)
-                GPIO.output(DRIVE_4, GPIO.LOW)
-
+        if power > 20:
+            fwleft.start(power-20)
+            rvleft.start(0)
+        if power < 0:
+            fwleft.start(0)
+            rvleft.start(power-20)
 #ESC Key
     if ord(k) == 27:
+                GPIO.cleanup()
                 exit()
     else:
-                time.sleep(.1)
-                GPIO.output(DRIVE_1, GPIO.LOW)
-                GPIO.output(DRIVE_2, GPIO.LOW)
-                GPIO.output(DRIVE_3, GPIO.LOW)
-                GPIO.output(DRIVE_4, GPIO.LOW)
+        if power > -1:
+            rvleft.start(power)
+            rvright.start(power)
+            fwleft.start(0)
+            fwright.start(0)
+        if power < 0:
+            fwleft.start(-power)
+            fwright.start(-power)
+            rvleft.start(0)
+            rvright.start(0)
+    return power
+
+power = 0
+while True:
+    power = drive(power)
 
 
-def main():
-    while True:
-        get()
-
-if __name__=='__main__':
-    main()
